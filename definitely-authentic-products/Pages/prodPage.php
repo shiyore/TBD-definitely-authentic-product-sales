dc0b45fe6d9c53c94ce2abe65f75d7516adce6dd
loki <shiyore@gmail.com>
2020-02-15 22:07:36 -0700

    Milestone 2 (Mk. 7)
    Completed the individual product page php file (prodPage.php).

--- definitely-authentic-products/Pages/prodPage.php
+++ definitely-authentic-products/Pages/prodPage.php
@@ -1,5 +1,6 @@
 <html lang="en">
-    <?php require_once "../Classes/Database.php";?>
+    <?php require_once "../Classes/Database.php";
+    require_once "../Scripts/utility.php";?>
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
@@ -10,26 +11,31 @@
 </head>
 <body>
     <?php 
-    $product = $product_ID;
+    include("fragments/navbar.php");
+    $product = $_GET['product_ID'];
     $db = new Database();
     $conn = $db->getConnected();
     $sql = "SELECT * FROM products WHERE product_ID = $product";
     $data = $conn->query($sql);
-    $info = ($row['name'], $row['price'], $row['description'], $row['desc_image']);
+    $row = $data->fetch_assoc();
+    $info = array($row['name'], $row['price'], $row['description'], $row['desc_image']);
     ?>
     <div class="container">
             <div class="row">
                 <div class="col-sm-6">
                     <h1><?php echo "$info[0]"?></h1>
+                    <hr>
                     <h2><?php echo "$info[2]"?></h2>
                 </div>
                 <div class="col-sm-6" style="border-style: solid; border-width: 1px;">
-                    <img style = "-ms-interpolation-mode: bicubic;" src="<?php $info[3]?>">
-                    <h2><?php echo "$info[1]"?></h2>
-                </div>
-                <div class="col-sm-2">
-                    <img src="https://i.redd.it/7fneh737oox21.png" >
+                    <?//php echo $info[3];?>
+                    <h2><?php echo "$".$info[1];?></h2>
+                    <hr>
+                    <img style = "-ms-interpolation-mode: bicubic;" src="<?php echo $info[3];?>">
+                    
                 </div>
             </div>
         </div>
+    </body>
+</html>
     
