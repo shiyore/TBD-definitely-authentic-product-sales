a28b5b6ec520d8a8823fff57e9cc6728e3bae96e
loki <shiyore@gmail.com>
2020-02-19 16:28:03 -0700

    Redirects 1
    added redirect folder

--- definitely-authentic-products/index.php
+++ definitely-authentic-products/index.php
@@ -13,9 +13,9 @@
 <?php include("Pages/fragments/navbar.php"); ?>
   
 <div class="container">
-  <h3>Collapsible Navbar</h3>
-  <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).
-  <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+  <h3>We Are Not Scamming You Inc.</h3>
+  <p>We are here to meet all of your needs.
+  <p>Always at your service.</p>
 </div>
 
 </body>
